026bc08e77c90221bd07aac787332a22d6e0f843
Dimka785 <dimka_krb@inbox.ru>
2015-04-06 19:01:59 +0300

    Task

--- PhpstormProjects/Task/js/scripts.js
+++ PhpstormProjects/Task/js/scripts.js
@@ -8,4 +8,15 @@ $(document).ready(function(){
     });$('.login, .login-form').mouseleave(function(){
         $('.login-form').slideUp();
     });
+    var cutfulltext2 = $('.story').text()
+    function cutfunk(b) {
+        var fulltext = b.text()
+        var newtext = fulltext.substr(0, 155);
+        b.text(newtext + '...');
+    }
+    $('#password').blur(function(){
+        if ($(this).val().length <= 5) {
+            alert("Enter 6 characters!");
+        }
+    });
 });
