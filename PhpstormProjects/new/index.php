22d9bdfa187038bd321e26c78a8c61452a29d73b
Dimka785 <dimka_krb@inbox.ru>
2015-04-09 12:59:39 +0300

    1

---
+++ PhpstormProjects/new/index.php
@@ -0,0 +1,14 @@
+<?php
+switch ($i) {
+    case "яблоко":
+        echo "i это яблоко";
+        break;
+    case "шоколадка":
+        echo "i это шоколадка";
+        break;
+    case "пирог":
+        echo "i это пирог";
+        break;
+
+}
+?>
