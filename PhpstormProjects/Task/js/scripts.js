a4542faad3822dc596736c1f596641c28b3c8023
Dimka785 <dimka_krb@inbox.ru>
2015-04-16 11:46:52 +0300

    Task

--- PhpstormProjects/Task/js/scripts.js
+++ PhpstormProjects/Task/js/scripts.js
@@ -8,15 +8,54 @@ $(document).ready(function(){
     });$('.login, .login-form').mouseleave(function(){
         $('.login-form').slideUp();
     });
-    var cutfulltext2 = $('.story').text()
-    function cutfunk(b) {
-        var fulltext = b.text()
-        var newtext = fulltext.substr(0, 155);
-        b.text(newtext + '...');
-    }
+
     $('#password').blur(function(){
         if ($(this).val().length <= 5) {
             alert("Enter 6 characters!");
         }
+    });
+    var cutfulltext = $('.text1').text()
+    var cutfulltext1 = $('.text2').text()
+    var cutfulltext2 = $('.text3').text()
+    function cutfunk(b){
+        var fulltext = b.text()
+        var newtext =fulltext.substr(0,155);
+        b.text(newtext+ '...');
+    }
+
+    cutfunk($('.text1'));
+    cutfunk($('.text2'));
+    cutfunk($('.text3'));
+
+
+
+    $('.click').click(function() {
+        $('#popupWrapper').show();
+        $('.popupTitle').text($('.title1').text());
+        $('.popupdata').text($('.data1').text());
+        $('.fulltext').text( cutfulltext);
+
+    });
+    $('.click1').click(function() {
+        $('#popupWrapper').show();
+        $('.fulltext').text($('.text2').text());
+        $('.popupTitle').text($('.title2').text());
+        $('.popupdata').text($('.data2').text());
+        $('.fulltext').text(cutfulltext1);
+    });
+    $('.click2').click(function() {
+        $('#popupWrapper').show();
+        $('.fulltext').text($('.text3').text());
+        $('.popupTitle').text($('.title3').text());
+
+        $('.fulltext').text(cutfulltext2);
+    });
+    $('#closeMe').click(function () {
+        $('#popupWrapper').hide();
+
+    });
+
+    $(function() {
+        $( "#datepicker" ).datepicker();
     });
 });
